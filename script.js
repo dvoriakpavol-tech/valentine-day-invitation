0ba7ef20c41e68da068f0374a96ed700f7ed4570
dvoriakpavol-tech <dvoriakpavol@gmail.com>
2026-02-13 13:21:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,22 @@ let leftPos = 0
 let bottomPos = 0
 document.getElementById("yessir").addEventListener("click", function() {
 
-  document.getElementById("mainImage").src =
-    "https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExMTA1ZnQwdTk5YmYxM2Q1eHg3Y2RzeDJzM2lheTRhNWdwZTY5MWM4NyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/jNJW9Bj6vVXIERUgK3/giphy.gif";
+  // nastaví nový obrázok ako pozadie celej stránky
+  document.body.style.backgroundImage =
+    "url('https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExMTA1ZnQwdTk5YmYxM2Q1eHg3Y2RzeDJzM2lheTRhNWdwZTY5MWM4NyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/jNJW9Bj6vVXIERUgK3/giphy.gif')";
+  document.body.style.backgroundSize = "cover";
+  document.body.style.backgroundPosition = "center";
 
-  setTimeout(function() {
-    document.getElementById("messageText").innerHTML =
-      "Yesss 😍 vedel som to! ❤️";
-  }, 1000);
+  // vytvorí nový text
+  const newText = document.createElement("h1");
+  newText.innerText = "Yesss 😍 vedel som to! ❤️";
+  newText.style.color = "white";
+  newText.style.textAlign = "center";
+  newText.style.marginTop = "40vh";
+  newText.style.fontSize = "40px";
+  newText.style.textShadow = "2px 2px 10px black";
+
+  document.body.appendChild(newText);
 
 });
 document.getElementById('nooo').addEventListener('mouseover', () => {
