d988ce16f328328c50d644591117ea3b15e32812
dvoriakpavol-tech <dvoriakpavol@gmail.com>
2026-02-13 13:17:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 let leftPos = 0
 let bottomPos = 0
-document.getElementById('yessir').addEventListener('click', () => {
-    alert('Pozývam ťa na rande, 14.februára. ')
-})
+document.getElementById("yessir").addEventListener("click", function() {
+
+  document.getElementById("mainImage").src =
+    "https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExMTA1ZnQwdTk5YmYxM2Q1eHg3Y2RzeDJzM2lheTRhNWdwZTY5MWM4NyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/jNJW9Bj6vVXIERUgK3/giphy.gif";
+
+  setTimeout(function() {
+    document.getElementById("messageText").innerHTML =
+      "Yesss 😍 vedel som to! ❤️";
+  }, 1000);
+
+});
 document.getElementById('nooo').addEventListener('mouseover', () => {
     leftPos = Math.floor(Math.random() * (50 - (-50) + 1)) + (-50);
     bottomPos = Math.floor(Math.random() * (100 - (-100) + 1)) + (-100);
