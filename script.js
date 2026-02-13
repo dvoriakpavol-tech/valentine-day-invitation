c72b5469195569261a8e21266c712660e6578e6e
dvoriakpavol-tech <dvoriakpavol@gmail.com>
2026-02-13 15:05:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ document.getElementById("yessir").addEventListener("click", function() {
   container.style.backgroundSize = "cover";
   container.style.backgroundPosition = "center";
   const text = document.createElement("h2");
-  text.innerText = "Yesss 😍 vedel som to! ❤️";
+  text.innerText = "Klikla si na nie, mám pravdu?";
   text.style.color = "white";
   text.style.fontSize = "42px";
   text.style.textShadow = "2px 2px 10px black";
